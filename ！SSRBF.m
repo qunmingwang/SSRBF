769d01870e144db65ba748ef128797e3d466c466
qunmingwang <71642787+qunmingwang@users.noreply.github.com>
2021-05-18 20:35:53 +0800

    Update ！SSRBF.m

--- ！SSRBF.m
+++ ！SSRBF.m
@@ -3,13 +3,20 @@
 %%%%When using the code, please cite the fowllowing papers
 %%%%Q. Wang, L. Wang, Z. Li, X. Tong, P. M. Atkinson. Spatial¨CSpectral Radial Basis Function-Based Interpolation for Landsat ETM+ SLC-Off Image Gap Filling, 2020.
 
-load SLCoff SLCoff
-load known known
+% Parameters
+%
+%    w          Half the window side
+%    s          The number of similar pixels per moving window
+%    sigma1     The empirically determined parameter for the spatial-based RBF
+%    sigma2     The empirically determined parameter for the spectral-based RBF
 
 w=17;
 s=20;
 sigma1=50;
 sigma2=0.1;
+
+load SLCoff SLCoff
+load known known
 
 [gapfilled]=SSRBF_function(SLCoff,known,w,s,sigma1,sigma2);
 
