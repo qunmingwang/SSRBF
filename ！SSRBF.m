70b65da2b5de20e6d94a989458ca5d601a7f8727
qunmingwang <71642787+qunmingwang@users.noreply.github.com>
2021-05-18 20:32:56 +0800

    Update ！SSRBF.m

--- ！SSRBF.m
+++ ！SSRBF.m
@@ -1,9 +1,15 @@
 %%%%This is the code for SSRBF produced by Dr Qunming Wang; Email: wqm11111@126.com
 %%%%Copyright belong to Qunming Wang
 %%%%When using the code, please cite the fowllowing papers
-%%%%Q. Wang, L. Wang, Z. Li, X. Tong, P. M. Atkinson. Spatial¨CSpectral Radial Basis Function-Based Interpolation for Landsat ETM+ SLC-Off Image Gap Filling, 2020.
+%%%%Q. Wang, L. Wang, Z. Li, X. Tong, P. M. Atkinson. SpatialÂ¨CSpectral Radial Basis Function-Based Interpolation for Landsat ETM+ SLC-Off Image Gap Filling, 2020.
 
 load SLCoff SLCoff
 load known known
-[gapfilled]=SSRBF_function(SLCoff,known);
+
+w=17;
+s=20;
+sigma1=50;
+sigma2=0.1;
+
+[gapfilled]=SSRBF_function(SLCoff,known,w,s,sigma1,sigma2);
 
