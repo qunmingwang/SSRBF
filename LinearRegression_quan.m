453143dffdd71acda6f53b8d7b5c6b7077aef474
qunmingwang <71642787+qunmingwang@users.noreply.github.com>
2021-05-18 20:30:28 +0800

    Update LinearRegression_quan.m

--- LinearRegression_quan.m
+++ LinearRegression_quan.m
@@ -3,10 +3,10 @@ function [A,B]=LinearRegression_quan(y,x)
  for i=1:band
     x_ceng=x(:,:,i);
     y_ceng=y(:,:,i); 
-    x_lie=x_ceng(:);%将矩阵按列存为列向量
+    x_lie=x_ceng(:);
     y_lie=y_ceng(:);
     X=[ones(length(y_lie),1),x_lie];
-    bb=regress(y_lie,X);  %[b,bint,r,rint,stats]----b:回归系数;bint:回归系数的区间估计;r:残差;rint:残差置信区间
+    bb=regress(y_lie,X);
     B(i)=bb(1);
     A(i)=bb(2);      
 end
